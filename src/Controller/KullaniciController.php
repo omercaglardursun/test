51e4e672b9a7435570e958f986b7364d6a83e3c5
infomedya-4 <infomedya-4@infomedya-4-iMac.local>
2019-07-29 16:12:20 +0300

    annotations router

--- src/Controller/KullaniciController.php
+++ src/Controller/KullaniciController.php
@@ -2,9 +2,27 @@
 
 
 namespace App\Controller;
+use Symfony\Component\DependencyInjection\Compiler\ResolveBindingsPass;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class KullaniciController
 {
+    /**
+     *  @\Symfony\Component\Routing\Annotation\Route("/")
+     */
 
+    public function homepage()
+    {
+        return new Response('Kullanıcı Ekranı');
+    }
+    /**
+     *  @\Symfony\Component\Routing\Annotation\Route("/hareketler/{fonk}")
+     */
+    public function show($fonk){
+            return new Response(sprintf(
+               'Hareket yoktur %s',
+                $fonk
+            ));
+    }
 }
